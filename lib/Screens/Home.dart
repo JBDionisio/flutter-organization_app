c78e0d0ccaa10fb410a81cf88999e856c83090f5
João Batista <jbdionisio@hotmail.com.br>
2019-02-12 23:22:54 -0200

    Layout test

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -9,7 +9,56 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello World!"),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+        centerTitle: true,
+        title: Text("ORGANIZATION", style: TextStyle(
+          color: Colors.white,
+        )),
+      ),
+      floatingActionButton: _buildFloatingButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: _buildBottomAppBar(),
+      body: Container(
+        padding: EdgeInsets.only(top: 5.0),
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      )
     );
   }
+
+  Widget _buildFloatingButton() {
+    return FloatingActionButton(
+      onPressed: null,
+      child: Icon(Icons.add, size: 30.0, color: Colors.black,),
+      backgroundColor: Colors.white70,
+    );
+  }
+  Widget _buildBottomAppBar() {
+    return Container(
+      height: 55.0,
+      child: BottomAppBar(
+        color: Color.fromRGBO(58, 66, 86, 1.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              iconSize: 30.0,
+              icon: Icon(Icons.wb_incandescent, color: Colors.white),
+              onPressed: () {},
+            ),
+            IconButton(
+              iconSize: 30.0,
+              icon: Icon(Icons.report, color: Colors.white),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
 }
