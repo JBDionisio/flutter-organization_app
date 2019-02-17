15ade6124138e77bb427a0ec57c8b6b8b585d5ea
João Batista <jbdionisio@hotmail.com.br>
2019-02-17 18:50:01 -0300

    #1 Possibility of choose color and icon

--- lib/screens/popups/ideasList.dart
+++ lib/screens/popups/ideasList.dart
@@ -6,6 +6,11 @@ class IdeasList extends StatefulWidget {
 }
 
 class _IdeasListState extends State<IdeasList> {
+
+  Color _color = Colors.blue[700];
+  IconData _icon = Icons.wb_incandescent;
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -31,12 +36,25 @@ class _IdeasListState extends State<IdeasList> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            new Container(
+              padding: EdgeInsets.only(left: 40.0, right:40.0, bottom: 20.0),
+              //color: Colors.black,
+              child: Center(
+                child: CircleAvatar(
+                  radius: 30.0,
+                  backgroundColor: _color,
+                  child: Icon(_icon,
+                      size: 35.0, color: Colors.white),
+                ),
+              ),
+
+            ),
             new Stack(
               alignment: Alignment.center,
               children: <Widget>[
                 new Container(
                   height: 60.0,
-                  padding: EdgeInsets.only(left: 19.8, right:19.8),
+                  padding: EdgeInsets.only(left: 20.0, right:16.0),
                   child: _icons(),
                 )
               ],
@@ -45,11 +63,12 @@ class _IdeasListState extends State<IdeasList> {
               children: <Widget>[
                 new Container(
                   height: 60.0,
-                  padding: EdgeInsets.only(left: 19.8, right:19.8),
+                  padding: EdgeInsets.only(left: 20.0, right:16.0),
                   child: _colors(),
                 )
               ],
-            )
+            ),
+
           ],
         ),
       )
@@ -60,38 +79,100 @@ class _IdeasListState extends State<IdeasList> {
     return ListView(
       scrollDirection: Axis.horizontal,
       children: <Widget>[
-        CircleAvatar(
-            child: Icon(Icons.build)
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.build;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.build)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.wb_incandescent)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.wb_incandescent;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.wb_incandescent)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.laptop_mac)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.laptop_mac;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.laptop_mac)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.alarm)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.alarm;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.alarm)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.call)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.call;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.call)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.home)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.home;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.home)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.audiotrack)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.audiotrack;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.audiotrack)
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-            child: Icon(Icons.done_all)
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _icon = Icons.done_all;
+            });
+          },
+          child: CircleAvatar(
+              child: Icon(Icons.done_all)
+          ),
         ),
-        SizedBox(width: 7.0,),
       ],
     );
   }
@@ -99,36 +180,99 @@ class _IdeasListState extends State<IdeasList> {
     return ListView(
       scrollDirection: Axis.horizontal,
       children: <Widget>[
-        CircleAvatar(
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.blue;
+            });
+          },
+          child: CircleAvatar(
             backgroundColor: Colors.blue,
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.black,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.blue[700];
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.blue[700],
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.tealAccent,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.greenAccent;
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.greenAccent,
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.deepPurple,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.green;
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.green,
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.amber,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.amber;
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.amber,
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.lightGreenAccent,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.red;
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.red,
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.red,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.deepPurple;
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.deepPurple,
+          ),
         ),
         SizedBox(width: 7.0,),
-        CircleAvatar(
-          backgroundColor: Colors.blueAccent,
+
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _color = Colors.black;
+            });
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.black,
+          ),
         ),
       ],
     );
